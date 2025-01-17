4757038cb7abcaead122108db116ccf20dc2632f
febriw <febriwanda26@gmail.com>
2025-01-18 02:33:27 +0700

    update(nuxt module): added nuxt font and setup global font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  css: ['@/assets/styles/main.scss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
+  css: ['@/assets/styles/main.scss'],
+  postcss: {
+    plugins: {
+      autoprefixer: {}
+    }
+  }
 })
