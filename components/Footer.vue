338a803854ea316bf8a675ec427f79f5ed224994
febriw <febriwanda26@gmail.com>
2025-01-18 03:32:23 +0700

    feat: added component footer and styles

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer>
+        <div id="content">
+            <p>&copy; 2024 MoovieTime. All rights reserved</p>
+            <img  src="~/assets/icon/MoovieTime-Logo-Grey.svg"/>
+            <p>Made With Nuxt JS 3 From Febri By Love</p>
+        </div>
+    </footer>
+</template>
