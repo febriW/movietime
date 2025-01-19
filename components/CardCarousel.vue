949ca03a52b273e77effdeccf4a7a9ab6cb80501
febriw <febriwanda26@gmail.com>
2025-01-19 21:29:53 +0700

    fix: update cardCarousel styling move to scss

--- components/CardCarousel.vue
+++ components/CardCarousel.vue
@@ -1,21 +1,20 @@
 <template>
-    <div id="card-carousel" class="relative flex w-[541.25px] h-[364.61px]">
+    <div id="card-carousel" class="carousel__wrapper">
         <img
             src="https://i.pinimg.com/236x/d9/bc/4b/d9bc4b468e5d73d5854a95a91901418b.jpg"
-            class="absolute w-1/2 object-cover"
         />
-        <div class="my-[20.26px] h-full">
-            <div class="bg-black justify-end h-full w-full ml-[25.64px] pr-[20.53px]">
-                <div class="grid grid-cols-2">
+        <div class="wrapper__content">
+            <div class="wrapper__content__right">
+                <div class="wrapper__content__right__grid">
                     <p></p>
                     <div class="py-[25.74px]">
-                        <div class="flex items-center mb-2">
-                            <span class="text-yellow-400 text-lg">★</span>
-                            <span class="text-lg font-bold ml-1 text-white">7.3</span>
+                        <div class="wrapper__content__right__grid--rating">
+                            <span class="star">★</span>
+                            <span class="rating">7.3</span>
                         </div>
-                        <h2 class="text-xl font-semibold text-white">Space Sweepers</h2>
-                        <p class="text-sm text-gray-400">2021 • Sci-Fi</p>
-                        <p class="text-sm mt-2 text-gray-300 flex-grow">
+                        <h2 class="title">Space Sweepers</h2>
+                        <p class="subtitle">2021 • Sci-Fi</p>
+                        <p class="description">
                         When the crew of a space junk collector ship called The Victory discovers
                         a humanoid robot named Dorothy that's known to be a weapon of mass
                         destruction, they get involved in a risky business deal which puts their
