87b5b794c7d6ec8854040ba9f4f97558029e7ef6
febriw <febriwanda26@gmail.com>
2025-01-21 01:08:59 +0700

    feat(component): dropdown and filtering component

---
+++ components/Dropdown.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const CategoriesMenu = [
+        { link:"/", name: "Popularity" },
+        { link:"/", name: "Popularity Ascending" },
+        { link:"/", name: "Popularity Descending" },
+        { link:"/", name: "Release Date Ascending" },
+        { link:"/", name: "Release Date Descending" },
+        { link:"/", name: "Rating Ascending" },
+        { link:"/", name: "Rating Descending" },
+    ]
+</script>
+<template>
+    <div class="dropdown__wrapper">
+        <div class="dropdown__wrapper--select group/dropdown">
+            <p>Popularity</p>
+            <img alt="icon"/>
+        </div>
+        <div class="group-hover/dropdown:block z-10 w-fit flex flex-col justify-start bg-white shadow-md gap-1">
+            <p v-for="menu in CategoriesMenu" :key="menu.name">
+                {{ menu.name }}
+            </p>
+        </div>
+    </div>
+</template>
