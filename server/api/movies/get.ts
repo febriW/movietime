674386282d347ad7d9a7057245886e40e68b98d8
febriw <febriwanda26@gmail.com>
2025-01-21 15:22:05 +0700

    fix: update params get movies api

--- server/api/movies/get.ts
+++ server/api/movies/get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => { 
     const query = getQuery(event)
     const config = useRuntimeConfig() 
-    const response = await $fetch(`${config.public.apiBaseUrl}/discover/movie?page=${query.page}`, { 
+    const response = await $fetch(`${config.public.apiBaseUrl}/discover/movie?page=${query.page}&sort_by=${query.sort_by}`, { 
         headers: { Authorization: `Bearer ${ config.apiKey }` } 
     }) 
     return response 
