4a45f147a0b2946d3a9a4a85440e40cfe0c0a21c
febriw <febriwanda26@gmail.com>
2025-01-21 15:20:33 +0700

    feat: added folder types to store types and interface typescript

---
+++ types/Movies.ts
@@ -0,0 +1,21 @@
+export interface IMovieRes {
+    backdrop_path: string
+    id: number
+    title: string
+    original_title: string
+    overview: string
+    poster_path: string
+    media_type: string
+    adult: boolean
+    original_language: string
+    genre_ids: number[]
+    popularity: number
+    release_date: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+  }
+  
+export interface IMovieWithGenre extends IMovieRes {
+    genre_name: string[]
+}
